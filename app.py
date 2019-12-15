3e8c689723f6a3ba7217fe47693aa29bff916a2d
lekgolo167 <167sonoma182@gmail.com>
2019-12-14 22:12:13 -0700

    made chart for day schedule

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, jsonify, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, time
 from flask_marshmallow import Marshmallow
 from sqlalchemy import Column, Integer, String, Float, DateTime
 
@@ -9,6 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///schedule.db'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 selDay = 0
 
 @app.cli.command('db_create')
@@ -46,7 +48,7 @@ class Cycle(db.Model):
 def index():
     global selDay
     if request.method == 'POST':
-        d = int(request.form['day'])
+        d = selDay
         h = int(request.form['hour'])
         m = int(request.form['min'])
         t = float(request.form['temperature'])
@@ -69,7 +71,7 @@ def index():
         print('\n\n selday {}'.format(selDay))
         cycles = Cycle.query.filter_by(d=selDay).all()
         print(cycles)
-        return render_template("index.html", cycles=cycles)
+        return render_template("index.html", cycles=cycles, days=DAYS, selDay=selDay)
 
 @app.route('/day', methods=['GET'])
 def day():
@@ -82,30 +84,51 @@ def day():
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Cycle.query.get_or_404(id)
+    cycle = Cycle.query.get_or_404(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        cycle.h = request.form['hour']
+        cycle.m = request.form['min']
+        cycle.t = request.form['temp']
 
         try:
             db.session.commit()
             return redirect('/')
         except:
-            return 'Faile to update task'
+            return 'Faile to update cycle'
     else:
-        return render_template("update.html", task=task)
+        return render_template("update.html", task=cycle)
+
+@app.route('/chart/<int:day>')
+def chart(day):
+    legend = 'Monthly Data'
+    # selDay
+    cycles = Cycle.query.filter_by(d=day).all()
+    tempAndTime = []
+    
+    for cycle in cycles:
+        tempAndTime.append((cycle.t,time(hour=cycle.h, minute=cycle.m)))
+
+    tempAndTime.sort(key=lambda x: x[1])
+    i = len(cycles)-1
+    print(cycles[i])
+    tempAndTime.append((tempAndTime[i][0],time(hour=23,minute=59)))
+    
+    print([cycle[1] for cycle in tempAndTime])
+    return render_template('chart.html', values=[cycle[0] for cycle in tempAndTime], labels=[cycle[1] for cycle in tempAndTime], legend=legend)
 
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Cycle.query.get_or_404(id)
+    cycle = Cycle.query.get_or_404(id)
 
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(cycle)
         db.session.commit()
         return redirect('/')
     except:
         return 'Could not delete task'
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
